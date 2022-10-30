410b71974150a18c8d0b26f9e282d2d8557e2eb0
mizso42 <jnzsmkls@gmail.com>
2022-10-30 20:53:02 +0100

    attempt for a helper class to handle big numbers

---
+++ Helper/big_num.cpp
@@ -0,0 +1,6 @@
+#include "big_num.h"
+#include <iostream>
+
+using namespace std;
+using namespace BN;
+
---
+++ Helper/big_num.h
@@ -0,0 +1,14 @@
+#ifndef BIG_NUM_H
+#define BIG_NUM_H
+
+#include <vector>
+
+namespace BN
+{
+    class bin
+    {
+        vector<bool> bits;
+    }
+}
+
+#endif
